8a3104bfb13adc3db680d7a95a56f2a06773c068
Alan Palazzolo <zzolo@147331.no-reply.drupal.org>
2012-03-26 15:00:42 -0500

    Updating JS with comments and fixing geocoding.

--- map/js/map-leaflet.js
+++ map/js/map-leaflet.js
@@ -1,44 +1,73 @@
+/**
+ * Redistricting map 2012
+ */
+
 var map;
 var interaction;
 var geocoder = new google.maps.Geocoder();
 var marker;
 
+// Namespace jQuery
+(function($) {
+
+/**
+ * Document ready event.
+ */
 $(document).ready(function() {
-    wax.tilejson('http://data.minnpost.s3.amazonaws.com/maps/leg_districts/leg_redistricting.json',
+  wax.tilejson('http://data.minnpost.s3.amazonaws.com/maps/leg_districts/leg_redistricting.json',
     function(tilejson) {
-        map = new L.Map('map-div')
-            .addLayer(new wax.leaf.connector(tilejson))
-            .setView(new L.LatLng(46.3, -94.2), 7);
-            //.setMaxBounds(new L.LatLngBounds(new L.LatLng(-97.7124, 43.125), new L.LatLng(-89.165, 49.5466)));
-        interaction = wax.leaf.interaction(map, tilejson);
-    });
-    $("#redist_search").submit(function(){geocode($("#redist_query").val());return false;});
+      map = new L.Map('map-div')
+        .addLayer(new wax.leaf.connector(tilejson))
+        .setView(new L.LatLng(46.3, -94.2), 7);
+        //.setMaxBounds(new L.LatLngBounds(new L.LatLng(-97.7124, 43.125), new L.LatLng(-89.165, 49.5466)));
+        
+      // Add wax interaction with UTFGrid data.
+      interaction = wax.leaf.interaction(map, tilejson);
+    }
+  );
+    
+  // Handle looking up address form.
+  $("#redist_search").submit(function() {
+    geocode($('#redist_query').val());
+    return false;
+  });
 });
 
-
+/**
+ * Geocode address from form.
+ */
 function geocode(query) {
-    if (typeof(query) == 'string') {
-        pattr = /\smn\s|\sminnesota\s/gi;
-        match = query.match(pattr);
-        if (!match) {
-            query = query + ' MN';
-        }
-        gr = { 'address': query };
-    } else {
-        gr = { 'location': query };
+  // Check for minnesota or MN, and if not, add it.
+  var gr = { 'location': query };
+  if (typeof(query) == 'string') {
+    var pattr = /\smn\s|\sminnesota\s/gi;
+    var match = query.match(pattr);
+    if (!match) {
+      query = query + ' MN';
     }
-    geocoder.geocode(gr, handle_geocode);
+    gr = { 'address': query };
+  }
+  
+  // Geocode and fire callback.
+  geocoder.geocode(gr, handle_geocode);
 }
- 
-function handle_geocode(results, status) {
-    lat = results[0].geometry.location.lat();
-    lng = results[0].geometry.location.lng();
-    
-    normalized_address = results[0].formatted_address;
-    $('#redist_query').val(normalized_address)
 
+/**
+ * Geocode callback.
+ */
+function handle_geocode(results, status) {
+  var lat = results[0].geometry.location.lat();
+  var lng = results[0].geometry.location.lng();
+  var normalized_address = results[0].formatted_address;
+  
+  // Update form value with nice address.
+  $('#redist_query').val(normalized_address)
+
+  // Add marker and center
 	var point = new L.LatLng(lat, lng);
-	arker = new L.Marker(point);
+	marker = new L.Marker(point);
 	map.addLayer(marker);
 	map.setView(point, 12);
 }
+
+})(jQuery);

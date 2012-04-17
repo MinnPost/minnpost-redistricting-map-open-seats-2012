1c787e437f520d48c532e29487a9a03545359fcb
Kevin Schaul <kschaul@minnpost.com>
2012-04-17 14:20:18 -0500

    Added project_dirs.py

---
+++ project_dirs.py
@@ -0,0 +1,17 @@
+# #
+# A list of maps to be used when calling deploy_all()
+MAPS_LIST = ['redistricting_pvi_house', 'redistricting_pvi_senate']
+
+# #
+# Directories for retrieving/storing various files
+MAPBOX_PROJECTS_DIRECTORY = '/Users/minnpostproduction/Documents/MapBox/project/'
+MAPBOX_EXPORT_DIRECTORY = '/Users/minnpostproduction/Documents/MapBox/export/'
+PROJECT_DIRECTORY = '/Users/minnpostproduction/data_projects/redistricting-map-open-seats-2012/'
+S3_DIRECTORY_S3CMD = 's3://data.minnpost/maps/leg_districts/'
+S3_DIRECTORY = 'data.minnpost/maps/leg_districts/'
+
+VERSION = '0.3'
+
+
+TEST = 'test'
+
